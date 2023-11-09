a4bdca6ff03a903be8a605f70073c87aecb01a0d
Eric Casanova <kilbane1407@gmail.com>
2023-11-09 18:16:58 -0300

    implementado diseño chatgpt pagina

---
+++ src/components/molecules/Label/Label.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Text from "../../atoms/Text/Text";
+import styles from "./label.module.css";
+const Label = ({ children, size }) => {
+  return (
+    <div className={styles.label}>
+      <Text size={size} color="transparent">
+        {children}
+      </Text>
+    </div>
+  );
+};
+
+export default Label;
--- src/pages/ChatBot/ChatBot.jsx
+++ src/pages/ChatBot/ChatBot.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import AsideNav from "../../components/organims/AsideNav/AsideNav";
 import styles from "./chatBot.module.css";
 import Input from "../../components/atoms/Input/Input";
+import Text from "../../components/atoms/Text/Text";
+import Label from "../../components/molecules/Label/Label";
 const ChatBot = () => {
   return (
     <div className={styles.chatBot}>
       <AsideNav />
       <main className={styles.main}>
-        <div className={styles.textShadow}></div>
-        <Input
-          placeholder={"Give Padda a business problem"}
-          id={"userPrompt"}
-          onChange={(id, value) => {}}
-          onError={(id, error) => {}}
-        />
+        <div className={styles.textShadow}>
+          <Text color="white" size={"3rem"}>
+            Paddawan
+          </Text>
+          <Label size={"1.5rem"}>Beta 0.0.1</Label>
+        </div>
+        <div className={styles.input}>
+          <Input
+            placeholder={"Give Padda a business problem"}
+            id={"userPrompt"}
+            onChange={(id, value) => {}}
+            onError={(id, error) => {}}
+          />
+        </div>
       </main>
     </div>
   );
